4145257d8b33b55ab68db095ba0eed406a73fef3
Jerry Jäppinen <eiskis@gmail.com>
2021-10-02 15:03:17 +0300

    Docs

--- src/index.js
+++ src/index.js
@@ -10,20 +10,24 @@ import sendSimpleApiRequest from './sendSimpleApiRequest'
 export default (appId) => {
   return {
 
-    fetch (...args) {
-      return sendShortAnswerRequest(appId, ...args)
-    },
-
-    fetchImage (...args) {
-      return sendSimpleApiRequest(appId, ...args)
-    },
-
     getClientUrl (...args) {
       return getClientUrl(...args)
     },
 
+    // Short answer API: get a quick, simple response
+
+    fetch (...args) {
+      return sendShortAnswerRequest(appId, ...args)
+    },
+
     getUrl (...args) {
       return getShortAnswerUrl(appId, ...args)
+    },
+
+    // Simple API: return image
+
+    fetchImage (...args) {
+      return sendSimpleApiRequest(appId, ...args)
     },
 
     getImageUrl (...args) {
